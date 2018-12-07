e2a0449f46fcbdac05260a93bdfa38d72db504c1
duda <mariaecabral@outlook.com>
2018-12-07 18:35:56 -0500

    another commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Loki",
+		photo: "https://www.theleathercity.com/wp-content/uploads/2017/08/Loki-Thor-Ragnarok-Leather-Jacket-1.jpeg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Tony Stark",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Robert_Downey_Jr._as_Iron_Man_in_Avengers_Infinity_War.jpg/200px-Robert_Downey_Jr._as_Iron_Man_in_Avengers_Infinity_War.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Peter Parker",
+		photo: "https://i.pinimg.com/originals/43/b5/2b/43b52b928e04c42882960180be4f2076.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Black Widow",
+		photo: "https://s1.r29static.com//bin/entry/1b5/720x864,80/1996479/image.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Squirrel Girl",
+		photo: "https://i.amz.mshcdn.com/QitFAeykm_6jIIjSqA8XPQtr1ow=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F531319%2F7b4c91d0-0ca4-4dae-9412-942e78a2afeb.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
